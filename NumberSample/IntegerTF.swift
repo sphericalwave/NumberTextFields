6e93ec58581824c82e3a9e26ef8ab12d78465347
aaronium112 <aaron@sphericalwave.com>
2023-01-12 16:11:57 -0400

    integerTF works in Fu3l, init only called once

--- NumberSample/IntegerTF.swift
+++ NumberSample/IntegerTF.swift
@@ -6,7 +6,6 @@
 //
 
 import os
-
 import SwiftUI
 
 struct IntegerTF: UIViewRepresentable {
@@ -44,40 +43,44 @@ struct IntegerTF: UIViewRepresentable {
         else {
             uiView.text = nil
         }
-        
-        //update swiftUi
-        DispatchQueue.main.async {
-            self.int = Int(removeFrmt)
-        }
     }
     
     func makeCoordinator() -> Coordinator {
         Self.logger.trace("makeCoordinator()")
-        return Coordinator(text: $text)
+        return Coordinator(text: $text, int: $int)
     }
     
     class Coordinator: NSObject, UITextFieldDelegate {
         private static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!,
                                            category: String(describing: IntegerTF.Coordinator.self))
         @Binding var text: String
+        @Binding var int: Int?
         
-        init(text: Binding<String>) {
+        init(text: Binding<String>, int: Binding<Int?>) {
             Self.logger.trace("init(text: Binding<String>)")
             self._text = text
+            self._int = int
         }
         
         func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
             if string != "" {
                 Self.logger.trace("add digit")
-                text = textField.text?.appending(string) ?? ""
+                self.text = textField.text?.appending(string) ?? ""
+                self.int = int(text: text)
             }
             else { //backspace case
                 Self.logger.trace("remove digit")
                 if let t = textField.text {
-                    text = String(t.dropLast())
+                    self.text = String(t.dropLast())
+                    self.int = int(text: text)
                 }
             }
             return false //swiftui updates uiTextField.text in updateUiView
+        }
+        
+        private func int(text: String) -> Int? {
+            let removeFrmt = text.filter (\.isWholeNumber)
+            return Int(removeFrmt)
         }
     }
 }
